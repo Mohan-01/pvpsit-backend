7afd7a33d29a77d772ea93db51988f08bb14b10b
Mohan <mohanvenkata123@gmail.com>
2023-09-20 20:29:35 +0530

    Updated the limit as 50MB

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ mongoose.connect(DB).then(() => console.log('Database connected successfully')).
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(cors({origin: true, credentials: true}));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
