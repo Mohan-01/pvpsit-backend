4cb78705fc6e401b206ccafa153930f15a5d5f04
Mohan <mohanvenkata123@gmail.com>
2023-09-21 10:50:12 +0530

    Updated cors function parameters

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json({limit: '50mb'}));
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: 'https://pvpsit.onrender.com',
+    credentials: true
 }));
 // app.use(cors({origin: true, credentials: true}));
 app.use(express.urlencoded({ extended: true }));
