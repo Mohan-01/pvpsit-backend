e5d59b230dae3a30c4f37ec544fabf3c71979c55
Mohan <mohanvenkata123@gmail.com>
2023-09-21 10:43:51 +0530

    Updated cors function

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json({limit: '50mb'}));
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 // app.use(cors({origin: true, credentials: true}));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
