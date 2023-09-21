47b950e37328180e9bfb2ebdb69bb2558783899e
Mohan <mohanvenkata123@gmail.com>
2023-09-21 11:18:20 +0530

    Added methods in cors function

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json({limit: '50mb'}));
 app.use(cors({
     origin: 'https://pvpsit.onrender.com',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     credentials: true
 }));
 // app.use(cors({origin: true, credentials: true}));
